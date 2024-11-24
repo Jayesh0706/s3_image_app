ce3880681b673a131a033116f6a43281813160b5
Jayesh Nalawade <92593054+Jayesh0706@users.noreply.github.com>
2024-11-24 23:39:07 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
 create database user_db;
+use user_db;
 create table users (user_id int primary key auto_increment ,name varchar(50), username varchar(50), email varchar(50), password nvarchar(255));
 create table user_images (user_id int , image_url nvarchar(255));
